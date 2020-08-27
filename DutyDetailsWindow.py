d81bb948578aac55c3c38b5cdd98da346d522fd6
patrick-deschambault <Patrick@DESKTOP-2V5A2VL>
2020-08-27 10:41:02 -0400

    Adding a selection date duty and tableView for tasks summary

--- DutyDetailsWindow.py
+++ DutyDetailsWindow.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import (QFormLayout, QApplication, QWidget, QDialog, QTextEdit, QVBoxLayout)
+from PyQt5.QtWidgets import (QFormLayout, QApplication, QDateEdit, QWidget, QDialog, QTextEdit, QHBoxLayout, QVBoxLayout, QTableWidget, QTableWidgetItem, QLabel)
+from PyQt5.QtCore import (QDate)
 
 from BacklogData import *
 from Task import *
@@ -10,26 +11,72 @@ class DutyDetailsWindow(QDialog):
 
         self.backlogData = iBacklogData
 
+        self.setWindowTitle("Duty details")
+        self.setMinimumWidth(500)
+
         mainLayout = QVBoxLayout(self)
 
-        self.textEdit = QTextEdit(self)
-        self.textEdit.setReadOnly(True)
-        self.doInit()
+        self.currDutyDate = self.backlogData.currDuty.date
+        currDutyDateLabel = QLabel("Workday date: ")
+        self.currDutyDateEdit = QDateEdit(self)
 
-        self.textEdit.autoFormatting()
+        self.currDutyDateLayout = QHBoxLayout(self)
+        self.currDutyDateLayout.addWidget(currDutyDateLabel)
+        self.currDutyDateLayout.addWidget(self.currDutyDateEdit)
 
-        mainLayout.addWidget(self.textEdit)
+        self.initCurrDutyDateEdit()
 
-    def doInit(self):        
-        
-        taskRegInDutyText = self.genTaskRegisteredString()
-        self.textEdit.setText(taskRegInDutyText)
+        self.dutySummaryTableWidget = QTableWidget(self)
+
+        summaryDailyTasksLabel = QLabel("Summary of daily tasks")
+        self.initDutySummaryTableWidget()
+
+        self.totalCompTime =  datetime.timedelta(seconds = self.backlogData.currDuty.totalTimeCompleted)
+        self.totalCompTimeLabel = QLabel("Total completed time: " + str(self.totalCompTime))
+
+        mainLayout.addLayout(self.currDutyDateLayout)
+        mainLayout.addWidget(summaryDailyTasksLabel)
+        mainLayout.addWidget(self.dutySummaryTableWidget)
+        mainLayout.addWidget(self.totalCompTimeLabel)
+
+
+    def initCurrDutyDateEdit(self):
+
+        splitDate = self.currDutyDate.split('-')
+
+        year = int(splitDate[0])
+        month = int(splitDate[1])
+        day = int(splitDate[2])
+
+        self.currDutyDateEdit.setDate(QDate(year,month,day))
+        self.currDutyDateEdit.setCalendarPopup(True)
+
+    def initDutySummaryTableWidget(self):
+
+        nbCol = 3
+        nbRow = len(self.backlogData.currDuty.tasksRegistered)
+
+        self.dutySummaryTableWidget.setColumnCount(nbCol)
+        self.dutySummaryTableWidget.setRowCount(nbRow)
+
+        listStr = list()
+        listStr = ["Title", "Project code", "Daily completed time"]
+
+        self.dutySummaryTableWidget.setHorizontalHeaderLabels(listStr)
+
+        for i in range(0, nbRow):
+
+            title = self.backlogData.currDuty.tasksRegistered[i].title
+            prjCode = self.backlogData.currDuty.tasksRegistered[i].prjCode
+            compTime = str(datetime.timedelta(seconds = self.backlogData.currDuty.tasksRegistered[i].completedTime))
+
+            self.dutySummaryTableWidget.setItem(i,0, QTableWidgetItem(title))
+            self.dutySummaryTableWidget.setItem(i,1, QTableWidgetItem(prjCode))
+            self.dutySummaryTableWidget.setItem(i,2, QTableWidgetItem(compTime))
 
     def genTaskRegisteredString(self):
-        
-        currDate = self.backlogData.currDuty.date
 
-        s = 'Time to log for the ' + currDate + ':\n'
+        s = 'Time to log for the ' + self.currDutyDate + ':\n'
         s += '\n'
 
         for currTask in self.backlogData.currDuty.tasksRegistered:
@@ -46,11 +93,5 @@ class DutyDetailsWindow(QDialog):
         s += str(datetime.timedelta(seconds = self.backlogData.currDuty.totalTimeCompleted))
 
         return s
-
-        
-
-
-        
-
 
         
